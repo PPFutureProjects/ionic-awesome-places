84c282f32dbf18bee188770009fcdebfb152869d
Kersuzan Anthony <kersuzan.anthony@gmail.com>
2017-02-03 14:14:25 +0100

    Creates app pages

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,11 +2,17 @@ import { NgModule, ErrorHandler } from '@angular/core';
 import { IonicApp, IonicModule, IonicErrorHandler } from 'ionic-angular';
 import { MyApp } from './app.component';
 import { HomePage } from '../pages/home/home';
+import { PlacePage } from "../pages/place/place";
+import { SetLocationPage } from "../pages/set-location/set-location";
+import { AddPlacePage } from "../pages/add-place/add-place";
 
 @NgModule({
   declarations: [
     MyApp,
-    HomePage
+    HomePage,
+	  PlacePage,
+	  SetLocationPage,
+	  AddPlacePage
   ],
   imports: [
     IonicModule.forRoot(MyApp)
@@ -14,7 +20,10 @@ import { HomePage } from '../pages/home/home';
   bootstrap: [IonicApp],
   entryComponents: [
     MyApp,
-    HomePage
+    HomePage,
+	  PlacePage,
+	  SetLocationPage,
+	  AddPlacePage
   ],
   providers: [{provide: ErrorHandler, useClass: IonicErrorHandler}]
 })
