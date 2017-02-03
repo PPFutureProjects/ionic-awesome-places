776c0a10fa13c6d7718b966f15de0af010e833a5
Kersuzan Anthony <kersuzan.anthony@gmail.com>
2017-02-03 15:35:18 +0100

    Adds geolocation feature

--- src/pages/add-place/add-place.ts
+++ src/pages/add-place/add-place.ts
@@ -1,6 +1,10 @@
 import { Component } from '@angular/core';
+
+import { Geolocation } from "ionic-native";
+import { ModalController, Loading, LoadingController, AlertController } from "ionic-angular";
+
 import { SetLocationPage } from "../set-location/set-location";
-import { ModalController } from "ionic-angular";
+
 import { Location } from "../../models/location.model";
 
 @Component({
@@ -11,8 +15,13 @@ export class AddPlacePage {
 
 	location: Location = new Location(40.7624234, -73.9759827);
 	locationIsSet: boolean = false;
+	loading: Loading;
 
-	constructor(private modalCtrl: ModalController) {}
+	constructor(
+		private modalCtrl: ModalController,
+	  private loadingCtrl: LoadingController,
+	  private alertCtrl: AlertController
+	) {}
 
 	onOpenMap() {
 		const modal = this.modalCtrl.create(SetLocationPage, { location: this.location, displayMarker: this.locationIsSet });
@@ -27,4 +36,35 @@ export class AddPlacePage {
 		modal.present();
 	}
 
+	onLocate() {
+		this.displayLoading();
+		Geolocation.getCurrentPosition()
+			.then(location => {
+				this.loading.dismiss();
+				this.location = new Location(location.coords.latitude, location.coords.longitude);
+				this.locationIsSet = true;
+			})
+			.catch(error => {
+				this.loading.dismiss();
+				this.displayError(error.message || 'Cannot get your location!');
+			});
+	}
+
+	private displayError(message: string) {
+		const alert = this.alertCtrl.create({
+			title: 'Location Error',
+			message: message,
+			buttons: ['OK']
+		});
+
+		alert.present();
+	}
+
+	private displayLoading() {
+		this.loading = this.loadingCtrl.create({
+			content: 'Getting your current location...'
+		});
+
+		this.loading.present();
+	}
 }
